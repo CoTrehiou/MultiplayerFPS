43c677ec7d99caa3cb1515cea974c856d15068bc
Papouboy <corentin.trehiou@wanadoo.fr>
2020-06-03 10:15:36 +0200

    [UPDATE] EP01

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _speed = 5f;
+
+    PlayerMotor _motor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _xMovement = Input.GetAxisRaw("Horizontal");
+        float _zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movementHorizontal = transform.right * _xMovement;
+        Vector3 _movementVertical = transform.forward * _zMovement;
+
+        Vector3 _velocity = Vector3.Normalize(new Vector3(_xMovement,0,_zMovement)) * _speed;
+
+        _motor.Move(_velocity);
+    }
+}
